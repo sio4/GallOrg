c7a6e3dece854a7ab18efd75f0a379279edf9bc7
Yonghwan SO <sio4@users.sf.net>
2010-09-30 20:16:28 +0900

    prepare basic structure of 'gallorg' project.

---
+++ src/org/scinix/android/gallorg/GallOrgInfo.java
@@ -0,0 +1,13 @@
+package org.scinix.android.gallorg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GallOrgInfo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
