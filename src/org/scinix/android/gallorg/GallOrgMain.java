214756c817942066fb11414715ce3744a4ffd8ac
Yonghwan SO <sio4@users.sf.net>
2011-06-26 15:49:53 +0900

    renamed and modified.

--- src/org/scinix/android/gallorg/GallOrgMain.java
+++ src/org/scinix/android/gallorg/GallOrgMain.java
@@ -3,7 +3,7 @@ package org.scinix.android.gallorg;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class GallOrgInfo extends Activity {
+public class GallOrgMain extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
